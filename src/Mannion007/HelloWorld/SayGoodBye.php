e6279117972975484d05179f4aecf306474c6af5
James Mannion <james@mannionking.com>
2015-04-15 02:02:45 +0100

    New file

---
+++ src/Mannion007/HelloWorld/SayGoodBye.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: james
+ * Date: 4/15/15
+ * Time: 2:01 AM
+ */
+
+namespace Mannion007\HelloWorld;
+
+
+class SayGoodBye
+{
+    public static function sayGoodbye()
+    {
+        return 'Goodbye!';
+    }
+}
