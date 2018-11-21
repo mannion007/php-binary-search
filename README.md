c104697972275a49f0aed42a089494e300cf60dd
Antoine Theriault-Brunet <antoine.theriault-brunet@dealerbreacher.com>
2018-11-21 14:10:50 -0500

    Update readme

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
 PHP Binary Search
 =================
-[![Build Status](https://travis-ci.org/mannion007/php-binary-search.svg?branch=master)](https://travis-ci.org/mannion007/php-binary-search)
 
-This is a composer package which wraps up the algorithm written by Michael King from ([mkwd.net](http://mkwd.net)).
-By performaing a binary search rather than a linear search, this package provides functionality to perform searches through ordered arrays with greatly imroved performance over PHP's [array_search](http://php.net/manual/en/function.array-search.php)
-Installation
+Fork from https://github.com/mannion007/php-binary-search for PHP 7
 ============
-To install, use Composer thusly:
+To install :
 ~~~~
-composer require mannion007/php-binary-search
+composer require AntoineTB/php-binary-search
 ~~~~
 License
 =======
--- test/ArraySearchTest.php
+++ test/ArraySearchTest.php
@@ -1,102 +1,73 @@
 <?php
+declare(strict_types=1);
+namespace AntoineTB\PhpBinarySearch\Tests;
 
-namespace Mannion007\PhpBinarySearch\Tests;
+use AntoineTB\PhpBinarySearch\ArraySearch;
+use PHPUnit\Framework\TestCase;
 
-use Mannion007\PhpBinarySearch\ArraySearch;
-use phpunit\framework\TestCase;
 
-class ArraySearchTest extends TestCase
-{
-    public function testBinarySearchWhenNeedleDoesNotExistInArrayWithNoDupes()
-    {
-        $needle = 'bradley@gmail.com';
-        $haystack = array(
-            'aaron@gmail.com',
-            'james@gmail.com',
-            'john@gmail.com',
-            'michael@gmail.com',
-            'peter@gmail.com',
-        );
-
-        $this->assertEquals(
-            -1,
-            ArraySearch::binarySearch($needle, $haystack, 'strcmp', count($haystack) -1, 0, false)
-        );
+final class ArraySearchTest extends TestCase {
+  public function testBinarySearchWhenNeedleDoesNotExistInArrayWithNoDupes(){
+    $needle = 'bradley@gmail.com';
+    $haystack = array(
+      'aaron@gmail.com',
+      'james@gmail.com',
+      'john@gmail.com',
+      'michael@gmail.com',
+      'peter@gmail.com',
+    );
+    $this->assertEquals(-1, ArraySearch::binarySearch($needle, $haystack, 'strcmp', count($haystack) -1, 0, false));
+  }
+  public function testBinarySearchWhenNeedleDoesNotExistInArrayWithDupes(){
+    $needle = 'bradley@gmail.com';
+    $haystack = array(
+      'aaron@gmail.com',
+      'james@gmail.com',
+      'john@gmail.com',
+      'michael@gmail.com',
+      'peter@gmail.com',
+      'peter@gmail.com',
+    );
+    $this->assertEquals(-1, ArraySearch::binarySearch($needle, $haystack, 'strcmp', count($haystack) -1, 0, true));
+  }
+  public function testBinarySearchWhenNeedleExistsInArrayWithNoDupes(){
+    $haystack = array(
+      'aaron@gmail.com',
+      'james@gmail.com',
+      'john@gmail.com',
+      'michael@gmail.com',
+      'peter@gmail.com',
+    );
+    foreach ($haystack as $key => $needle) {
+      $this->assertEquals($key, ArraySearch::binarySearch($needle, $haystack, 'strcmp', count($haystack) -1, 0, false));
     }
-
-    public function testBinarySearchWhenNeedleDoesNotExistInArrayWithDupes()
-    {
-        $needle = 'bradley@gmail.com';
-        $haystack = array(
-            'aaron@gmail.com',
-            'james@gmail.com',
-            'john@gmail.com',
-            'michael@gmail.com',
-            'peter@gmail.com',
-            'peter@gmail.com',
-        );
-
-        $this->assertEquals(
-            -1,
-            ArraySearch::binarySearch($needle, $haystack, 'strcmp', count($haystack) -1, 0, true)
-        );
-    }
-
-    public function testBinarySearchWhenNeedleExistsInArrayWithNoDupes()
-    {
-        $haystack = array(
-            'aaron@gmail.com',
-            'james@gmail.com',
-            'john@gmail.com',
-            'michael@gmail.com',
-            'peter@gmail.com',
-        );
-
-        foreach ($haystack as $key => $needle) {
-            $this->assertEquals(
-                $key,
-                ArraySearch::binarySearch($needle, $haystack, 'strcmp', count($haystack) -1, 0, false)
-            );
-        }
-    }
-
-    public function testBinarySearchWhenNeedleExistsInArrayWithDupesWhenNeedleIsNotOneOfTheDupes()
-    {
-        $needle = 'john@gmail.com';
-        $haystack = array(
-            'aaron@gmail.com',
-            'james@gmail.com',
-            'james@gmail.com',
-            'john@gmail.com',
-            'michael@gmail.com',
-            'michael@gmail.com',
-            'peter@gmail.com',
-            'peter@gmail.com',
-        );
-
-        $this->assertEquals(
-            3,
-            ArraySearch::binarySearch($needle, $haystack, 'strcmp', count($haystack) -1, 0, false)
-        );
-    }
-
-    public function testBinarySearchWhenNeedleExistsInArrayWithDupesWhenNeedleIsOneOfTheDupes()
-    {
-        $needle = 'peter@gmail.com';
-        $haystack = array(
-            'aaron@gmail.com',
-            'james@gmail.com',
-            'james@gmail.com',
-            'john@gmail.com',
-            'michael@gmail.com',
-            'michael@gmail.com',
-            'peter@gmail.com',
-            'peter@gmail.com',
-        );
-
-        $this->assertEquals(
-            6,
-            ArraySearch::binarySearch($needle, $haystack, 'strcmp', count($haystack) -1, 0, false)
-        );
-    }
+  }
+  public function testBinarySearchWhenNeedleExistsInArrayWithDupesWhenNeedleIsNotOneOfTheDupes(){
+    $needle = 'john@gmail.com';
+    $haystack = array(
+      'aaron@gmail.com',
+      'james@gmail.com',
+      'james@gmail.com',
+      'john@gmail.com',
+      'michael@gmail.com',
+      'michael@gmail.com',
+      'peter@gmail.com',
+      'peter@gmail.com',
+    );
+    $this->assertEquals(3, ArraySearch::binarySearch($needle, $haystack, 'strcmp', count($haystack) -1, 0, false));
+  }
+  public function testBinarySearchWhenNeedleExistsInArrayWithDupesWhenNeedleIsOneOfTheDupes(){
+    $needle = 'peter@gmail.com';
+    $haystack = array(
+      'aaron@gmail.com',
+      'james@gmail.com',
+      'james@gmail.com',
+      'john@gmail.com',
+      'michael@gmail.com',
+      'michael@gmail.com',
+      'peter@gmail.com',
+      'peter@gmail.com',
+    );
+    $this->assertEquals(6, ArraySearch::binarySearch($needle, $haystack, 'strcmp', count($haystack) -1, 0, false));
+  }
 }
